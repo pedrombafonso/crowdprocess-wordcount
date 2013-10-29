22af49e294c1ccffc6fa528312afdcd2fa335fc5
pedrombafonso <mail@pedroafonso.net>
2013-10-29 11:11:56 +0000

    Wordcount example: program and data

---
+++ program.js
@@ -0,0 +1,23 @@
+function isWord(w) {
+  return w.length > 0;
+}
+
+function Run (data) {
+  var s = data.toLowerCase();
+
+  //split by comma, period, single space
+  a = s.split(/[ ,.!?:;]/);
+
+  var r = {}
+  while(a.length) {
+    var w = a.pop();
+    if (!isWord(w)) continue;
+
+    if (r[w]) {
+      r[w]++;
+    } else {
+      r[w] = 1;
+    }
+  }
+  return r;
+}
