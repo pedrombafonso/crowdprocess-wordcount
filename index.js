96ce9d0e81d2a29640b2a5a7dd3db61788d15b1f
pedrombafonso <mail@pedroafonso.net>
2013-10-29 11:41:59 +0000

    using crowdprocess module

---
+++ index.js
@@ -0,0 +1,41 @@
+var crowdprocess = require('crowdprocess');
+var fs = require('fs');
+
+//Task bid
+var bid = 1;
+
+var program = fs.readFileSync('run.js', 'utf8');
+var textLines = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+crowdprocess(program, bid, function(err, task){
+
+  var tSent = 0;
+  var tRcvd = 0;
+  var wordCounter = {};
+
+  for (var i = 0; i < textLines.length; i++) {
+
+    //Data unit object
+    dataUnit = textLines[i];
+    task.write(dataUnit);
+    tSent++;
+  }; 
+
+  //Deal with results
+  task.on('result', function(result){
+    tRcvd++;
+
+    var words = result;
+    for (w in words) {
+      if (wordCounter[w]){
+        wordCounter[w] += words[w];
+      } else {
+        wordCounter[w] = words[w];
+      }
+    };
+
+    if (tSent === tRcvd) {
+      console.log('Words counted:\n', wordCounter);
+    };
+  });
+});
